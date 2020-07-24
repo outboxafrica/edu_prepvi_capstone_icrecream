a2643cb31868838909e46730bad7a79ab40019a4
Jonathan-Meya <jonathanmeya@gmail.com>
2020-07-24 13:22:53 +0300

    first commit

---
+++ scripts/contactpage.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    alert("Thanks for subscribing");
+}
+
+function sendMessage() {
+    alert("Your message has been submitted");
+}
---
+++ scripts/foodmenu.js
@@ -0,0 +1,6 @@
+
+function myFunction() {
+    alert("Thanks for subscribing");
+}
+
+document.getElementById('order').onclick = function(){alert('Your order has been recorded');};
